c3262242ce75a124ab9708514c3ddff81e8edb18
Kirill <aboba@mail.com>
2026-04-09 15:09:02 +0700

    glava 8

---
+++ glava8/test_code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "checksum.h"
+#include "encrypt.h"
+
+int main()
+{
+char s[] = "Скажи друг и проходи";
+encrypt(s);
+printf("Зашифровано в '%s'\n", s);
+printf("Контрольная сумма %i\n", checksum(s));
+encrypt(s);
+printf("Расшифровано обратно в '%s'\n", s);
+printf("Контрольная сумма %i\n", checksum(s));
+return 0;
+}
